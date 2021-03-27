0ebf4c753e24c73e1182ddba751d6796942b937f
JzsCodegame <69696812+JzsCodegame@users.noreply.github.com>
2021-03-27 16:35:02 -0400

    Update SignupTestcase.java

--- src/test/java/TestModel/SignupTestcase.java
+++ src/test/java/TestModel/SignupTestcase.java
@@ -34,10 +34,10 @@ public class SignupTestcase {
 WebDriverManager.getInstance(CHROME).setup();
 	  // System.setProperty("webdriver.chrome.driver", "chromedriver");
       
-	    ChromeOptions ChromeOptions = new ChromeOptions();
-ChromeOptions.addArguments("--headless", "window-size=1024,768", "--no-sandbox");
-driver = new ChromeDriver(ChromeOptions);
-        
+	   // ChromeOptions ChromeOptions = new ChromeOptions();
+//ChromeOptions.addArguments("--headless", "window-size=1024,768", "--no-sandbox");
+//driver = new ChromeDriver(ChromeOptions);
+        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         driver.get("http://automationpractice.com/index.php");
