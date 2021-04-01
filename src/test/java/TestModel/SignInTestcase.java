a60ac375c76d88b8b9d0030f70c92bf252f7625f
JzsCodegame <you@example.comyou@example.com>
2021-04-01 12:18:59 -0400

    5th commit

--- src/test/java/TestModel/SignInTestcase.java
+++ src/test/java/TestModel/SignInTestcase.java
@@ -3,6 +3,7 @@ package TestModel;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
@@ -57,7 +58,13 @@ public class SignInTestcase {
 	 // System.setProperty("webdriver.chrome.driver", "/usr/lib/chromium-browser/chromedriver");
 	  //ChromeOptions ChromeOptions = new ChromeOptions();
 	  //ChromeOptions.addArguments("--headless", "window-size=1024,768", "--no-sandbox");
-    	  WebDriverManager.chromedriver().setup();
+    	ChromeOptions opt = new ChromeOptions();
+    	opt.setBinary("/usr/bin/google-chrome");  //chrome binary location specified here
+    	opt.addArguments("start-maximized");
+    	opt.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+    	opt.setExperimentalOption("useAutomationExtension", false);
+    	WebDriver driver = new ChromeDriver(opt); 
+    	WebDriverManager.chromedriver().setup();
 	driver = new ChromeDriver();
 		//driver = new RemoteWebDriver(new URL("http://192.168.1.114:4444/wd/hub"), ChromeOptions);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
